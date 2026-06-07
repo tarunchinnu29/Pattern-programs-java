a62a533e790b38965b6d16eba6d5c30b73e13595
tarunchinnu29 <tarunchinnu29@gmail.com>
2026-06-07 22:13:57 +0530

    rotated_by_180

---
+++ rotated_by_180.java
@@ -0,0 +1,21 @@
+class rotated_by_180
+{
+	public static void main(String args[])
+{
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			if(j<=4-i+1)
+			{
+				System.out.print(" ");
+			}
+			else
+			{
+				System.out.print("*");
+			}}
+			System.out.println();
+		}
+}
+}
+
